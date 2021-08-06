8d0545a0b68578321da208f7b9941cef40a0eedf
Blockies <87968309+blockies-xyz@users.noreply.github.com>
2021-08-06 09:33:05 +0530

    Update myLib.js

--- libs/myLib.js
+++ libs/myLib.js
@@ -1,4 +1,76 @@
-import fs from "fs"
+var getDirectoryContent = function(dirPath) {
+
+    /* 
+
+        get list of files and directories from given dirPath and all it's sub directories
+
+        NON RECURSIVE ALGORITHM
+
+        By. Dreamsavior
+
+    */
+
+    var RESULT = {'files':[], 'dirs':[]};
+
+    var fs = fs||require('fs');
+
+    if (Boolean(dirPath) == false) {
+
+        return RESULT;
+
+    }
+
+    if (fs.existsSync(dirPath) == false) {
+
+        console.warn("Path does not exist : ", dirPath);
+
+        return RESULT;
+
+    }
+
+    var directoryList = []
+
+    var DIRECTORY_SEPARATOR = "\\";
+
+    if (dirPath[dirPath.length -1] !== DIRECTORY_SEPARATOR) dirPath = dirPath+DIRECTORY_SEPARATOR;
+
+    directoryList.push(dirPath); // initial
+
+    while (directoryList.length > 0) {
+
+        var thisDir  = directoryList.shift(); 
+
+        if (Boolean(fs.existsSync(thisDir) && fs.lstatSync(thisDir).isDirectory()) == false) continue;
+
+        var thisDirContent = fs.readdirSync(thisDir);
+
+        while (thisDirContent.length > 0) { 
+
+            var thisFile  = thisDirContent.shift(); 
+
+            var objPath = thisDir+thisFileif (fs.existsSync(objPath) == false) continue;
+
+            if (fs.lstatSync(objPath).isDirectory()) { // is a directory
+
+                let thisDirPath = objPath+DIRECTORY_SEPARATOR; 
+
+                directoryList.push(thisDirPath);
+
+                RESULT['dirs'].push(thisDirPath);
+
+            } else  { // is a file
+
+                RESULT['files'].push(objPath); 
+
+            } 
+
+        } 
+
+    }
+
+    return RESULT;
+
+}
 function hello(){ Bot.sendMessage("Hello from lib!") }
 
 function goodbye(name){ Bot.sendMessage("Goodbye, " + name) }
